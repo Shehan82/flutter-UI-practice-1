90802f04aa4c012ae9506b177d2122927003837d
shehan82 <shehansandeepa82@gmail.com>
2021-05-04 10:57:21 +0530

    created the top bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,6 +8,36 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        child: ListView(
+          physics: ClampingScrollPhysics(),
+          children: [
+            Container(
+              padding: EdgeInsets.all(15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.view_headline_outlined,
+                    size: 30,
+                    color: Colors.grey,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/tt.jpg'))),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
